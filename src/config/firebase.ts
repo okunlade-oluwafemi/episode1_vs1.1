c3a7a314e80e748dc3c9b320b473d786c1fb928d
Femi Okunlade <107478675+okunlade-oluwafemi@users.noreply.github.com>
2024-06-17 14:22:26 +0100

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -7,12 +7,6 @@ import { getFirestore } from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCuY7UIrwjS09xn1prU9RNxFDPuJ2LNpoc",
-  authDomain: "react-course-21ba1.firebaseapp.com",
-  projectId: "react-course-21ba1",
-  storageBucket: "react-course-21ba1.appspot.com",
-  messagingSenderId: "313075358205",
-  appId: "1:313075358205:web:0363af2cbadc962d646194"
 };
 
 // Initialize Firebase
